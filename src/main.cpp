ffdbd6dfcf64cf7c527d8edb5cbf436fe93889f6
Wyatt Coppinger <wcopping@asu.edu>
2017-06-17 01:22:37 -0700

    Add Makefile Skeleton
    Makefile is now in src directory but does not compile main.cpp
    correctly.
    
    [ticket: X]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
+/* Main.cpp
+ * The entry point of the program
+ */
+
+// Need to make sure these are all included in the g++ compiler command line
+// You need to link these with -I or -l or $(gpk-config) or something
+// Try to do this with a Makefile so you don't have to redo it over again
 #include <iostream>
 #include "SDL.h"
+#include "graphics.h"
 
 int main()
 {
-  std::cout << "hi" << std::endl;
+  Graphics graphics;
+  while (true) {
+    
+  }
   return 0;
 }
