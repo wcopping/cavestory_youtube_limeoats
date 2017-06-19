a8a7f16dc554c88a936f91452b17821a24dcc36e
Wyatt Coppinger <wcopping@asu.edu>
2017-06-19 13:14:50 -0700

    content folder; sprite class; graphics updated
    Added a lot of rendering methods for positioning things from a sprite
    sheet (in this case, we loaded mychar.png in /content/sprites; the
    sprite class is a neat little rectangular thing that creates a sprite,
    given x,y,width,height,etc from a sprite sheet; There are still many
    problems in this build and currently the character does NOT load onto
    the window (however it is suppose to at this point).
    
    [ticket: X]

--- header/graphics.h
+++ header/graphics.h
@@ -1,6 +1,13 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+/* Graphics class
+ * Holds all information dealing with graphics for the game
+ */
+
+#include <map>
+#include <string>
+
 struct SDL_Window;
 struct SDL_Renderer;
 
@@ -9,9 +16,45 @@ class Graphics
 public:
   Graphics();
   ~Graphics();
+
+  // This has to do with us not wanting to constantly re-load images if
+  // unnecessary?
+  /* SDL_Surface* loadImage
+   * Loads an image into the _spriteSheets map if it doesn't already exist
+   * As a result, each image will only ever be loaded once
+   * Returns the image from the map regardless of whether or not it was just
+   * loaded
+   */
+  SDL_Surface* loadImage(const std::string &filePath);
+
+
+  /* void blitSurface
+   * Draws a texture (just part of [a sourceRectangle] the source) to a certain 
+   * part of the screen (the destinationRectangle)
+   */
+  void blitSurface(SDL_Texture* source, SDL_Rect* sourceRectangle,
+                   SDL_Rect* destinationRectangle);
+
+  /* void flip
+   * Renders everything to the screen
+   */
+  void flip();
+
+  /* void clear
+   * Clears the screen
+   */
+  void clear();
+  
+  /* SDL_Renderer* getRenderer 
+   * Returns the renderer
+   */
+  SDL_Renderer* getRenderer() const;
+
+
 private:
   SDL_Window* _window;
   SDL_Renderer* _renderer;
+  std::map<std::string, SDL_Surface*> _spriteSheets;
 };
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -30,6 +30,7 @@ void Game::gameLoop()
   Graphics graphics;
   Input input;
   SDL_Event event;
+  this->_player = Sprite(graphics, "content/sprites/myChar.png", 0, 0, 16, 16, 100, 100);
 
   int LAST_UPDATE_TIME = SDL_GetTicks();
   // Start the game loop
@@ -57,12 +58,18 @@ void Game::gameLoop()
     int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
     this->update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
     LAST_UPDATE_TIME = CURRENT_TIME_MS;
+
+    this->draw(graphics);
   }
 }
 
 void Game::draw(Graphics &graphics)
 {
+  graphics.clear();
 
+  this->_player.draw(graphics, 100, 100);
+
+  graphics.flip();
 }
 
 void Game::update(float elapsedTime)
